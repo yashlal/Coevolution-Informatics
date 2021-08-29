abe653a4532a4c8c61491512ebc81860b1c67e50
yashlal <yash.lal.m@gmail.com>
2021-08-29 16:15:14 -0700

    MFE one file pre-MP working save:

--- MFE_calc.py
+++ MFE_calc.py
@@ -3,9 +3,11 @@ import random as rd
 import tqdm
 import pickle
 import numpy as np
+import multiprocessing
+import modules
+from Bio import SeqIO
 
-bases = ['A','G','C','T']
-
+bases = ["A",'G','C','T']
 def process_val(raw_input):
     val = []
     s=0
@@ -31,29 +33,6 @@ def mutate(pxn, full_seq):
 
     return new_s
 
-def main_MFE_func(s, pxns):
-    ref_input = ''.join(s)
-    p = Popen('C:\Program Files (x86)\ViennaRNA Package\RNAfold.exe', stdin=PIPE, stdout=PIPE)
-    reference = list(p.communicate(ref_input.encode())[0].decode())
-    ref_val = process_val(reference)
-
-    MFE_sum = 0
-
-    pbar = tqdm.tqdm(range(len(pxns)))
-    for dummy_iter in pbar:
-
-        x = pxns[dummy_iter]
-        inp = ''.join(mutate(x, s))
-
-        p = Popen('C:\Program Files (x86)\ViennaRNA Package\RNAfold.exe', stdin=PIPE, stdout=PIPE)
-        ans = list(p.communicate(inp.encode())[0].decode())
-
-        value_for_dict = process_val(ans)
-        MFE_sum += value_for_dict-ref_val
-
-    avg_mfe_pert = MFE_sum/len(pxns)
-    return avg_mfe_pert, ref_val
-
 def MFE_func_all(s, pxns):
     ref_input = ''.join(s)
     p = Popen('C:\Program Files (x86)\ViennaRNA Package\RNAfold.exe', stdin=PIPE, stdout=PIPE)
@@ -61,20 +40,90 @@ def MFE_func_all(s, pxns):
     ref_val = process_val(reference)
 
     MFE_list = []
-
-    pbar = tqdm.tqdm(range(len(pxns)))
-    for dummy_iter in pbar:
-
-        x = pxns[dummy_iter]
+    pbar = tqdm.trange(len(pxns))
+    for pbar_iter in pbar:
+        x = pxns[pbar_iter]
         if x=='BLANK':
             MFE_list.append(np.nan)
         else:
-            inp = ''.join(mutate(x, s))
-
+            inp = ''.join(mutate(x,s))
             p = Popen('C:\Program Files (x86)\ViennaRNA Package\RNAfold.exe', stdin=PIPE, stdout=PIPE)
             ans = list(p.communicate(inp.encode())[0].decode())
-
             value_for_dict = process_val(ans)
             MFE_list.append(value_for_dict-ref_val)
 
     return MFE_list, ref_val
+
+def run_sequence(seq):
+    site_nums = []
+    pure_seq_l = []
+
+    for i in range(len(seq)):
+        if seq[i] not in blanks:
+            site_nums.append(i)
+            pure_seq_l.append(seq[i])
+
+    sites_2 = []
+    nonsites_2 = []
+    for site in sites:
+        try:
+            sites_2.append(site_nums.index(site))
+        except:
+            sites_2.append('BLANK')
+    for nonsite in nonsites:
+        try:
+            nonsites_2.append(site_nums.index(nonsite))
+        except:
+            nonsites_2.append('BLANK')
+
+    MFE_pos, rv = MFE_func_all(pure_seq_l, sites_2)
+    MFE_neg, rv = MFE_func_all(pure_seq_l, nonsites_2)
+
+    return MFE_pos, MFE_neg, rv
+
+blanks = ['-','.']
+species = ['Cyanobacteria']
+b = []
+for spec in species:
+    with open(f'Results\{spec}Results_E_0.116.pickle', 'rb') as handle:
+        b.append(pickle.load(handle)[0.8])
+
+sites = []
+nonsites = []
+
+for i in b[0]:
+    if type(i)==list:
+        sites.append(i)
+    else:
+        nonsites.append(i)
+
+sites=list(modules.flatten(sites))
+
+sites=sites[0:10]
+nonsites=nonsites[0:10]
+
+with open('data\SILVA_138.1_Cyanobacteria.fasta') as handle:
+    data = list(SeqIO.parse(handle, "fasta"))
+
+bad_seqs = [51, 74, 75, 76, 93, 113, 118, 120]
+
+if __name__=='__main__':
+
+    results_l = []
+    index_col = []
+
+
+    for j in range(50):
+        if j not in bad_seqs:
+            index_col.append(j)
+            print(f'Sequence {j}/50')
+            sequence = str(data[j].seq)
+            p_vals, n_vals, ref = run_sequence(sequence)
+            print(p_vals, n_vals, ref)
+            results_l.append([ref]+p_vals+n_vals)
+
+    columns_labels = ['REFERENCE']+['S'+str(_) for _ in sites]+['NS'+str(n_) for n_ in nonsites]
+    results_df = pd.DataFrame(results_l, index=index_col, columns=columns_labels)
+
+    results_df.to_excel('CB_MFE50.xlsx')
+    print(results_df)
