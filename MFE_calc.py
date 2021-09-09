c1a8c1a87585ce204d0b7c96e5d020a3f3fae6c4
yashlal <yash.lal.m@gmail.com>
2021-09-08 21:30:43 -0700

    Updated MFE calc python file (no need for labels until graphing step)

--- MFE_calc.py
+++ MFE_calc.py
@@ -10,8 +10,11 @@ from Bio import SeqIO
 from tqdm.contrib.concurrent import process_map
 import pandas as pd
 
-
 bases = ['A','G','C','T']
+safe = ['A','G','C','T', '-', '.']
+blanks = ['-','.']
+species = ['Cyanobacteria', 'Fusobacteriota']
+
 def process_val(raw_input):
     val = []
     s=0
@@ -56,7 +59,7 @@ def MFE_func_all(s, pxns):
 
     return MFE_list, ref_val
 
-def run_sequence(seq):
+def run_sequence(seq, sites):
     site_nums = []
     pure_seq_l = []
 
@@ -66,65 +69,53 @@ def run_sequence(seq):
             pure_seq_l.append(seq[i])
 
     sites_2 = []
-    nonsites_2 = []
     for site in sites:
         try:
             sites_2.append(site_nums.index(site))
         except:
             sites_2.append('BLANK')
-    for nonsite in nonsites:
-        try:
-            nonsites_2.append(site_nums.index(nonsite))
-        except:
-            nonsites_2.append('BLANK')
 
-    MFE_pos, rv = MFE_func_all(pure_seq_l, sites_2)
-    MFE_neg, rv = MFE_func_all(pure_seq_l, nonsites_2)
+    MFE_vals, rv = MFE_func_all(pure_seq_l, sites_2)
 
-    return MFE_pos, MFE_neg, rv
-
-blanks = ['-','.']
-species = ['Bacteroidota']
-b = []
-for spec in species:
-    with open(f'Results\{spec}Results_E_0.116.pickle', 'rb') as handle:
-        b.append(pickle.load(handle)[0.8])
-
-sites = []
-nonsites = []
-
-for i in b[0]:
-    if type(i)==list:
-        sites.append(i)
-    else:
-        nonsites.append(i)
-
-sites=list(modules.flatten(sites))
-
-sites=sites
-nonsites=nonsites
-
-with open('data\SILVA_138.1_Bacteroidota.fasta') as handle:
-    data = list(SeqIO.parse(handle, "fasta"))[0:50]
-
-bad_seqs = [0, 1, 5, 17, 18, 19, 24, 26, 29, 31, 36, 39, 40, 48, 56, 61, 72, 84, 92, 93, 98]
+    return MFE_vals, rv
 
 if __name__=='__main__':
+    b = []
+    for spec in species:
+        with open(f'Results\{spec}Results_E_0.116.pickle', 'rb') as handle:
+            b.append(pickle.load(handle)[0.95])
 
-    results_l = []
-    index_col = []
+        sites = list(modules.flatten(b))
 
-    pool_input = []
+        sites=sites
 
-    for j in range(35):
-        if j not in bad_seqs:
-            index_col.append(j)
-            print(f'Sequence {j}/50')
-            pool_input.append(str(data[j].seq))
+        with open(f'data\SILVA_138.1_{spec}.fasta') as handle:
+            data = list(SeqIO.parse(handle, "fasta"))
 
-    pool_output = process_map(run_sequence, pool_input)
-    formatted_output = [[z]+x+y for x,y,z in pool_output]
-    columns_labels = ['REFERENCE']+['S'+str(_) for _ in sites]+['NS'+str(n_) for n_ in nonsites]
-    results_df = pd.DataFrame(formatted_output, index=index_col, columns=columns_labels)
+        bad_seqs = []
+        for sequence_ind in range(100):
+            s=0
+            for char in str(data[sequence_ind].seq):
+                if char not in safe:
+                    s=1
+            if s:
+                bad_seqs.append(sequence_ind)
 
-    results_df.to_excel('BD_MFE25.xlsx')
+        results_l = []
+        index_col = []
+
+        pool_input = []
+
+        for j in range(50):
+            if j not in bad_seqs:
+                index_col.append(j)
+                print(f'Sequence {j}/50')
+                pool_input.append((str(data[j].seq), sites))
+
+        with multiprocessing.Pool() as pool:
+            pool_output = pool.starmap(run_sequence, pool_input)
+        formatted_output = [[y]+x for x,y in pool_output]
+        columns_labels = ['REFERENCE']+[str(_) for _ in sites]
+        results_df = pd.DataFrame(formatted_output, index=index_col, columns=columns_labels)
+
+        results_df.to_excel(f'MFE/{spec}_MFE50.xlsx')
