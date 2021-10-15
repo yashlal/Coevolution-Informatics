d79f8276cc8cc6f1ff3a1a40e290e2a1cf72bca4
yashlal <yash.lal.m@gmail.com>
2021-10-15 14:57:38 -0700

    Testing beta version of MFE ordering function

--- network.py
+++ network.py
@@ -9,9 +9,9 @@ import multiprocessing
 from itertools import combinations
 import pickle
 import random
+# import mendotaMFE
 
 species_list = ['Fusobacteriota', 'Cyanobacteria', 'Bacteroidota']
-# species_list = ['Fusobacteriota']
 epsilon = 0.116
 gammas = [0.95, 0.90, 0.85]
 
@@ -31,7 +31,7 @@ def get_sites(species, gamma):
     sites = list(filter(lambda x: type(x)==list, b1[gamma]))
     return sites
 
-def ordering(data, sites):
+def entr_ordering(data, sites):
 	t_len = len(sites)
 	results = []
 	for cluster in sites:
@@ -66,12 +66,55 @@ def ordering(data, sites):
 
 	return results
 
-for species in species_list:
-	data = load_data(species=species, epsilon=epsilon)
-	for gamma in gammas:
-		sites = get_sites(species=species, gamma=gamma)
-		print(sites)
-		print(len(data), len(data[0]))
-		RES = ordering(data, sites)
-		with open(f'Results/InfRankings/{species}_E{epsilon}_G{gamma}_Ranking.pickle', 'wb') as handle:
-			pickle.dump(RES, handle)
+def mfe_ordering(data, sites):
+	results = []
+	for cluster in sites:
+		results.append([])
+		for step in range(len(cluster)):
+			print(results)
+			calc_list = []
+			for el in cluster:
+				seqs = []
+				c=0
+				i=0
+				while c<20:
+					seq = data[i]
+					if not any([seq[el]=='B']):
+						c += 1
+						i += 1
+						seqs.append(seq)
+						continue
+					i += 1
+
+				s=0
+				for seq in seqs:
+					ref_val = RNA.fold(seq)
+					exp_val = RNA.fold(mutate(el, seq))
+					s += abs(ref_val-exp_val)
+
+				calc_list.append((el,s/50))
+
+				calc_list.sort(key=lambda x: x[1])
+
+			cluster.remove(calc_list[-1][0])
+			results.append(calc_list[-1][0])
+
+	for x in results:
+		x.reverse()
+
+	return results
+
+if __name__=='__main__':
+	# for species in species_list:
+	# 	data = load_data(species=species, epsilon=epsilon)
+	# 	for gamma in gammas:
+	# 		sites = get_sites(species=species, gamma=gamma)
+	# 		print(sites)
+	# 		print(len(data), len(data[0]))
+	# 		RES = entr_ordering(data, sites)
+	# 		with open(f'Results/InfRankings/{species}_E{epsilon}_G{gamma}_Ranking.pickle', 'wb') as handle:
+	# 			pickle.dump(RES, handle)
+
+	data = load_data(species=species_list[0], epsilon=epsilon)
+	sites = get_sites(species=species_list[0], gamma=gammas[0])
+	print(mfe_ordering(data,sites))
