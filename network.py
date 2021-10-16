b3e7cb1234dc7d622a411b1a09edfa2d9e42cfc3
yashlal <yash.lal.m@gmail.com>
2021-10-15 18:00:02 -0700

    Revised, normalized ranking informatic experiment

--- network.py
+++ network.py
@@ -11,6 +11,7 @@ import pickle
 import random
 
 species_list = ['Fusobacteriota', 'Cyanobacteria', 'Bacteroidota']
+# species_list = ['Fusobacteriota']
 epsilon = 0.116
 gammas = [0.95, 0.90, 0.85]
 
@@ -36,22 +37,33 @@ def ordering(data, sites):
 	for cluster in sites:
 		print(str(int(sites.index(cluster))+1) + f'/{t_len}')
 		results.append([])
+		unsorted_l = []
+
+		X = []
+		for j in cluster:
+			X.append([d[j] for d in data])
+		H_all = joint_entr(*X)
+
 		for i in range(len(cluster)):
 			new_cluster = cluster.copy()
 			new_cluster.remove(cluster[i])
-			X = []
-			for j in cluster:
-				X.append([d[j] for d in data])
+
 			Y = []
 			for k in new_cluster:
 				Y.append([d2[k] for d2 in data])
-			if type(X[0])!=list:
-				X=[X]
-			if type(Y[0])!=list:
-				Y=[Y]
-			H_x = joint_entr(*X)
 			H_y = joint_entr(*Y)
-			results[-1].append(H_x-H_y)
+
+			H_ind = shannon_entr([d3[i] for d3 in data])
+
+			statistic = (H_ind-(H_all-H_y))/(H_ind)
+			unsorted_l.append(statistic)
+
+		sorted_l = unsorted_l.copy()
+		sorted_l.sort()
+
+		for el in sorted_l:
+			results[-1].append(cluster[unsorted_l.index(el)])
+
 	return results
 
 for species in species_list:
@@ -61,6 +73,5 @@ for species in species_list:
 		print(sites)
 		print(len(data), len(data[0]))
 		RES = ordering(data, sites)
-
 		with open(f'Results/InfRankings/{species}_E{epsilon}_G{gamma}_Ranking.pickle', 'wb') as handle:
 			pickle.dump(RES, handle)
