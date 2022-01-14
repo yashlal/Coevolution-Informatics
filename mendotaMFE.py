cc560b12c2029550b9947640e26326954d50b96b
yashlal <yash.lal.m@gmail.com>
2022-01-13 23:03:25 -0700

    big fix

--- mendotaMFE.py
+++ mendotaMFE.py
@@ -136,7 +136,7 @@ def run_ecoli(path='data/4ybb.fasta', max_iter=6, total=150):
         itervar2 = int(iter_num*(chunskize+1))
         iter_pairs = ecoli_pairs[itervar1:itervar2]
 
-        pool_input = [(ecoli_pair, ecoli_seq) for ecoli_pair in all_ecoli_pairs]
+        pool_input = [(ecoli_seq, ecoli_pair) for ecoli_pair in all_ecoli_pairs]
         with multiprocessing.Pool() as pool:
             pool_output = pool.starmap(ecoli_mp_func, pool_input)
 
