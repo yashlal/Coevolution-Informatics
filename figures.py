dd1fe4714deba201fb60c812551ca377055cd914
yashlal <yash.lal.m@gmail.com>
2022-01-15 16:34:43 -0700

    figures

--- figures.py
+++ figures.py
@@ -39,9 +39,7 @@ def mfe_metrics(values_list):
                     metric_val = E_ab-(E_a+E_b)
                     sum += metric_val
 
-                    choices = [E_ab-E_a, E_ab-E_b]
-
-                    possibilities.append(min(choices))
+                    possibilities.append(abs(E_ab))
     return abs(sum/9), min(possibilities)
 
 def get_dist_and_mfe_metric(items, coords, ref_l):
@@ -61,14 +59,30 @@ def get_dist_and_mfe_metric(items, coords, ref_l):
 
     return all_dist, all_mfe
 
-if __name__=='__main__':
-    spec = 'Cyanobacteria'
+def ecoli_get_dist_and_mfe_metric(items, coords, ref_l):
+    all_dist = []
+    all_mfe = []
+    for item in items:
+        pair, values = item
+
+        try:
+            dist = np.linalg.norm(coords[pair[0]]-coords[pair[1]])
+            all_dist.append(dist)
+
+            mfe = mfe_metrics(values)
+            all_mfe.append(mfe)
+        except ValueError:
+            continue
+
+    return all_dist, all_mfe
+
+def spec_figures(spec='Cyanobacteria', n=10000):
+
     with open(f'Results/Dump/{spec}_PW_MFE.pickle', 'rb') as handle:
         b = pickle.load(handle)
 
     coords, ref_l = get_reference_dist()
     count = 0
-    n=10000
 
     ss = b[0:n]
     sn = b[n:2*n]
@@ -95,3 +109,65 @@ if __name__=='__main__':
         plt.plot(rX,rP)
         plt.show()
         plt.clf()
+
+def ecoli_figures(type='hist'):
+    all_ecoli_data = []
+    for n in range(6):
+        with open(f'Results/Dump/ecoli_{n}.pickle', 'rb') as handle:
+            b = pickle.load(handle)
+            [all_ecoli_data.append(_) for _ in b]
+
+    coords, ref_l = get_reference_dist()
+    ecoli_dist, ecoli_mfe = ecoli_get_dist_and_mfe_metric(all_ecoli_data, coords, ref_l)
+
+    var1 = ecoli_dist
+    var2 = [el[0] for el in ecoli_mfe]
+    var3 = [el[1] for el in ecoli_mfe]
+
+    vars = [var1,var2,var3]
+    paths = ['Results/NewFigs/EColi/DistHist', 'Results/NewFigs/EColi/MFEAddHist', 'Results/NewFigs/EColi/MFECompHist']
+
+    if type=='hist':
+        for n in range(3):
+            var = vars[n]
+
+            if n == 0:
+                d = 0.1
+            else:
+                d = 0.05
+                var = list(filter(lambda x: x>0.001, var))
+            bins_input = np.arange(math.floor(min(var)), math.ceil(max(var)), d)
+
+            a, b, c = plt.hist(var, bins=bins_input)
+
+            plt.savefig(f'{paths[n]}/hist_nozero.png')
+            plt.savefig(f'{paths[n]}/hist_nozero.eps', format='eps')
+            plt.clf()
+
+    elif type=='loglin':
+        for n in range(3):
+            var = vars[n]
+
+            if n == 0:
+                d = 0.1
+            else:
+                d = 0.05
+                var = list(filter(lambda x: x>0.001, var))
+            bins_input = np.arange(math.floor(min(var)), math.ceil(max(var)), d)
+
+            a, b, c = plt.hist(var, bins=bins_input)
+            plt.clf()
+
+            X, Y = [], []
+            for i in range(len(a)):
+                if a[i]!=0:
+                    X.append(b[i])
+                    Y.append(np.log(a[i]))
+
+            plt.scatter(X,Y)
+            plt.show()
+    else:
+        pass
+
+if __name__=='__main__':
+    ecoli_figures('loglin')
